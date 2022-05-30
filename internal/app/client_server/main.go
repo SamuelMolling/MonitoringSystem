ad2b6a40d86647ed8782c78e62f3c204bca0bc50
Samuel Molling <samuel.molling@ifood.com.br>
2022-05-30 00:30:56 -0300

    message

--- internal/app/client_server/main.go
+++ internal/app/client_server/main.go
@@ -2,31 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"time"
+
+	"github.com/tarm/serial"
 )
 
 func main() {
-	// config := &serial.Config{
-	// 	Name:        "/dev/tty.usbmodem2103",
-	// 	Baud:        115200,
-	// 	ReadTimeout: 1,
-	// 	Size:        8,
-	// }
+	config := &serial.Config{
+		Name:        "/dev/tty.usbmodem2103",
+		Baud:        115200,
+		ReadTimeout: 1,
+		Size:        8,
+	}
 
-	// stream, err := serial.OpenPort(config)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	stream, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// buf := make([]byte, 1024)
+	buf := make([]byte, 1024)
 	conn, _ := net.Dial("tcp", "127.0.0.1:1234")
 
 	for {
 		time.Sleep(2 * time.Second)
-		// n, _ := stream.Read(buf)
-		// s := string(buf[:n])
-		s := "T:25.5,P:101302"
+		n, _ := stream.Read(buf)
+		s := string(buf[:n])
+		//s := "T:25.5,P:101302"
 		fmt.Fprintf(conn, s+"\n")
 		fmt.Println(s)
 	}
